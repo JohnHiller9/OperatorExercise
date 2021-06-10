a184b1728249966c83aa2091769f7e5db640b813
John Hiller <jdhiller@bellsouth.net>
2021-06-10 13:53:47 -0500

    Exercise1

--- repos/OperatorExercise/OperatorExercise/Program.cs
+++ repos/OperatorExercise/OperatorExercise/Program.cs
@@ -6,7 +6,31 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Explicit typing -
+            string variableName = "Hello";
+            //Inferred typing -
+            var varName = "Hello";
+
+
+            //----------Exercise 1----------
+            //Create 2 variables a and b, with a = 17 and b = 4.
+            var a = 17;
+            var b = 4;
+            //Create variables for addition, subtraction, and multiplication.
+            var plus = a + b;
+            var minus = a - b;
+            var times = a * b;
+            //Create variable named quotient that stores the division of a and b.
+            //Create another variable named remainder that stores the remainder using the % operator.
+            var quotient = a / b;
+            var remainder = a % b;
+            //Write out results using Console.WriteLine.
+            Console.WriteLine($"{a} + {b} = {plus}");
+            Console.WriteLine($"{a} - {b} = {minus}");
+            Console.WriteLine($"{a} * {b} = {times}");
+            Console.WriteLine($"{a} / {b} = {quotient} remainder {remainder}");
+
+
         }
     }
 }
